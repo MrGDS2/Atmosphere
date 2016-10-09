530fee522d56d8f56a408c08f8c3678e58e1e5d6
MrGDS2 <mrsonicthunder@gmail.com>
2016-10-09 18:50:51 -0400

    test
    Jay

---
+++ ProjectActive/Assets/Scripts/Moveuser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveuser : MonoBehaviour {
+
+
+    public float speed = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0,speed * Time.deltaTime); //moves object towards user
+
+    }
+}
+
+	
+	
+
