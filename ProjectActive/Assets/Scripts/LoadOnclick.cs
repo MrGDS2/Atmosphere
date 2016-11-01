acfe90a90fbb8d93b65ee9b1afca749ce4673da3
artonm <artonmirakaj@Arton.local>
2016-10-31 21:02:09 -0400

    update
    update

---
+++ ProjectActive/Assets/Scripts/LoadOnclick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnclick : MonoBehaviour {
+
+	public GameObject LoadingImage;
+
+	public void LoadScene(int level){
+
+		LoadingImage.SetActive (true);
+		Application.LoadLevel (level);
+	}
+}
