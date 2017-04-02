389bcc90ddbc1b9c78348187cc7e9590866184fa
MrGDS2 <mrsonicthunder@gmail.com>
2017-04-02 05:59:44 -0400

    Player 1 counts over the network
    All players can see player one's count

--- ProjectActive/Assets/Scripts/Network/PlayerCounting.cs
+++ ProjectActive/Assets/Scripts/Network/PlayerCounting.cs
@@ -8,49 +8,78 @@ public class PlayerCounting: Photon.MonoBehaviour {
     // Use this for initialization
     public Text CountingAsteroidText, CountingAsteroidText2;
     private int networkcount;
+   private string player1score, player2score;
+   private PhotonView view;
    int i;
    // public int number;
 	void Start () {
-     //   CountingAsteroidText = GameObject.Find("Playercount").GetComponentInChildren<Text>();
-       
+        //   CountingAsteroidText = GameObject.Find("Playercount").GetComponentInChildren<Text>();
+        view = new PhotonView();
+    
     }
 
 // Update is called once per frame
 void Update () {
+
+      
+
+
         if (photonView.isMine)
         {
             CountingAsteroidText.color = Color.green;
             CountingAsteroidText.text = SyncGazeExplosion.instance.Counting().ToString();
-        }
 
-       if(!photonView.isMine)
+          //  CountingAsteroidText2.text = player2score;//null?
+
+        }
+        else
         {
-            CountingAsteroidText2.color = Color.red;
-            CountingAsteroidText2.text = SyncGazeExplosion.Counter.ToString();
+            CountingAsteroidText2.color = Color.yellow;
+            CountingAsteroidText2.text = SyncGazeExplosion.instance.Counting().ToString();
         }
+    
 
-
+    
 
 
 	}
 
+
+
+
+
+
+
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
+
+       
         Debug.Log("OnPhotonSerializeView()");
         if (stream.isWriting == true)
-        {
+        {  //sends information to player 2 
             stream.SendNext(CountingAsteroidText.text);
+         
+
 
         }
         else  //issue
         {
 
-          CountingAsteroidText2.text= (string) stream.ReceiveNext();
+
+        //player 2 receives the information
+           CountingAsteroidText.text = (string)stream.ReceiveNext();
+          
+
         }
 
-        // ...
+
+
     }
-
-
-
+      
+    // ...
 }
+
+
+
+
+
